10c15ddf0b0fe6f6836fda374cf027d55dae3e6c
adarshrajnikant-beep <adarshrajnikant@gmail.com>
2026-08-23 04:13:48 +0000

    First working build - Vajra OS Launcher v0.1

---
+++ launcher/app/src/main/java/com/vajra/os/launcher/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.vajra.os.launcher
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Vajra OS - Hello World! 🇮🇳"
+        textView.textSize = 24f
+        setContentView(textView)
+    }
+}
